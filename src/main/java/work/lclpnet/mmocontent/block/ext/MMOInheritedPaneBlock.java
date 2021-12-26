17fb7fcd723cf408aa06169180af47a2a4cdddff
LCLP <lukas@lclpnet.work>
2021-12-26 16:46:07 +0100

    remove last render layer client call in shared code

--- src/main/java/work/lclpnet/mmocontent/block/ext/MMOInheritedPaneBlock.java
+++ src/main/java/work/lclpnet/mmocontent/block/ext/MMOInheritedPaneBlock.java
@@ -2,14 +2,10 @@ package work.lclpnet.mmocontent.block.ext;
 
 import net.minecraft.block.Block;
 import work.lclpnet.mmocontent.block.BlockStatesUtil;
-import work.lclpnet.mmocontent.client.render.block.MMORenderLayers;
-import work.lclpnet.mmocontent.util.Env;
 
 public class MMOInheritedPaneBlock extends MMOPaneBlock {
 
     public MMOInheritedPaneBlock(Block parent) {
         super(BlockStatesUtil.copyState(parent));
-
-        if (Env.isClient()) MMORenderLayers.inheritRenderLayer(this, parent);
     }
 }
